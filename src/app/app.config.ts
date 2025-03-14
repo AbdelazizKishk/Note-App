ead1dd2f9c0db2fd2902f96f80b743dce197f0d1
Ezz <Elhussien@DESKTOP-80TJDJV>
2025-03-14 03:09:13 +0300

    commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,7 @@ import {
   importProvidersFrom,
   provideZoneChangeDetection,
 } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { NgxSpinnerModule } from 'ngx-spinner';
 
 import { routes } from './app.routes';
@@ -23,7 +23,10 @@ import { loadingInterceptor } from './core/interceptors/loading/loading.intercep
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(
+      routes,
+      withInMemoryScrolling({ scrollPositionRestoration: 'top' })
+    ),
     provideClientHydration(withEventReplay()),
     provideHttpClient(
       withFetch(),
